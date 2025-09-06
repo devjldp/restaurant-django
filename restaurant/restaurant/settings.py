16648340d525e8bf15c0472d30242a23942ab1f3
jldpDev <joseluisdp297@gmail.com>
2025-09-06 12:09:44 +0100

    Set up postgres db

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -12,15 +12,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+# import packages
+import os
+from environs import Env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load the environment variables
+env = Env()
+env.read_env(os.path.join(BASE_DIR,'.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0ea(umd@4xtj@#%n@&g)b61=9!^-5tu1rre0k%+z9$!f4!s(&d'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,8 +81,13 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str("DB_NAME"),
+        'USER': env.str("DB_USER"),
+        'PASSWORD': env.int("DB_PASSWORD"),
+        'HOST': env.str("DB_HOST", "localhost"),
+        'PORT': env.int("DB_PORT", 5432)
+
     }
 }
 
