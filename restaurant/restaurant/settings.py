cb2beb0945f5a6bc19af9f236b8dd44e7d330780
jldpDev <joseluisdp297@gmail.com>
2025-09-13 10:24:06 +0100

    Templates and Static Files configuration

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -91,7 +91,7 @@ LOGIN_REDIRECT_URL = '/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -159,6 +159,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Find any static file in the global space
+STATICFILES_DIRS = [
+    BASE_DIR/"static"
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
