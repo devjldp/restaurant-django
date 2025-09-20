ba464479708d95b2196691f5e81aeec2e4cd8332
jldpDev <joseluisdp297@gmail.com>
2025-09-20 12:32:12 +0100

    Create urls menu app

--- restaurant/menu/urls.py
+++ restaurant/menu/urls.py
@@ -1,1 +1,14 @@
 # Define the urls for menu app
+#  import path 
+from django.urls import path
+
+# Import views from the app
+from . import views
+
+app_name = 'menu'
+
+# Define the url patterns
+urlpatterns = [
+    path('', views.select_all_menu, name='menu_list')
+    # define more paths - urls
+]
--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include('allauth.urls')),
+    path('menu/', include('menu.urls'))
 ]
