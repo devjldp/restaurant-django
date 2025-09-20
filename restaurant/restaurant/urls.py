50e1fbf680f3dbf4f78528bfea5f3972b832a8d3
jldpDev <joseluisdp297@gmail.com>
2025-09-20 14:23:08 +0100

    Add  settings for development

--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -17,9 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# This is only for development
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include('allauth.urls')),
     path('menu/', include('menu.urls'))
 ]
+
+# Only for development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
