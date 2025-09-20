28ebd1c960060182b034af66163f665bbf1eb0ee
jldpDev <joseluisdp297@gmail.com>
2025-09-20 12:19:17 +0100

    Create select_all_menu view

--- restaurant/menu/views.py
+++ restaurant/menu/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
+# import errors:
+from django.db import DatabaseError
+
+
+# Import the models we are going to use
+from .models import MenuItem
+# from home.models import  => importing form a different app
+
 # Create your views here.
+def select_all_menu(request):
+    """
+    This function retrieves all dishes from the database
+    return menu template / dish data
+    """
+    try:
+        # query = "Select * from menuITem inner join menuitem.catery = Category.id"
+        # connection to the databse
+        # cursor....
+        dishes = MenuItem.objects.all()
+        # var1 = data from table1
+        # var2 = data from table2
+        # for debugging
+        print(f"Retrieved {dishes.count()} from Database")
+    except DatabaseError as e:
+        print(f"There is an error in the query. Error: {e}")
+        dishes = []
+    
+    context = {
+        'menu': dishes,
+        #'customer' : var1
+        #'order' : var2
+    }
+    return render(request, 'menu.html', context)
