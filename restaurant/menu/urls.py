fced1a1b883e7dfe7986c993271c79a3ec3a4a63
jldpDev <joseluisdp297@gmail.com>
2025-09-20 14:23:28 +0100

    Create menu_detail template and url

--- restaurant/menu/urls.py
+++ restaurant/menu/urls.py
@@ -9,6 +9,7 @@ app_name = 'menu'
 
 # Define the url patterns
 urlpatterns = [
-    path('', views.select_all_menu, name='menu_list')
+    path('', views.select_all_menu, name='menu_list'),
     # define more paths - urls
+    path('<int:dish_id>/', views.menu_detail, name='menu_detail')
 ]
