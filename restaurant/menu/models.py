4a2b892459296c34c55f7c9542df1026c398a66b
jldpDev <joseluisdp297@gmail.com>
2025-09-24 12:26:05 +0100

    change Ingredient model field update_at to updated_at and migrate

--- restaurant/menu/models.py
+++ restaurant/menu/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now = True)
+    updated_at = models.DateTimeField(auto_now = True)
 
     def __str__(self):
         return self.name
