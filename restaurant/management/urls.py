9bbf0d1608f24fc27757ad27103e85f78084261e
jldpDev <joseluisdp297@gmail.com>
2025-09-27 15:25:21 +0100

    Create url for update view

--- restaurant/management/urls.py
+++ restaurant/management/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     # define more paths - urls
     path('dashboard/dishes/', views.management_select_all_dishes, name='list_dishes'),
     path('dashboard/add_dish/', views.management_add_dish, name='add_dish'),
-    path('dasshboard/delete_dish/<int:dish_id>/', views.management_delete_dish, name = 'delete_dish'),
+    path('dashboard/delete_dish/<int:dish_id>/', views.management_delete_dish, name = 'delete_dish'),
+    path('dashboard/update_dish/<int:dish_id>/',views.management_update_dish, name='update_dish')
     
 ]
