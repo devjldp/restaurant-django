69cd6964369b368cacdd0f0d036bf9af472fba43
jldpDev <joseluisdp297@gmail.com>
2025-09-27 11:56:51 +0100

    Create management_select_all_dishes view

--- restaurant/management/views.py
+++ restaurant/management/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth.decorators import login_required
 
+# import MenuItem model from menu app
+from menu.models import MenuItem
+
 # Create your views here.
 @login_required
 def post_login_redirect(request):
@@ -26,3 +29,20 @@ def management_home(request):
     if not request.user.is_superuser:
         return redirect('home:index')
     return render(request, 'dashboard.html')
+
+
+# Implement the CRUD using Django ORM
+# view to render all dishes - only the name of the dish
+
+@login_required
+def management_select_all_dishes(request):
+    # Get all object(dishes) in the menu: -> we need to import the models.
+    
+    # Use try and except to improve how to handle errors
+    dishes = MenuItem.objects.all()
+
+    context = {
+        'menu': dishes
+    }
+
+    return render(request, 'dashboard_dishes.html', context)
