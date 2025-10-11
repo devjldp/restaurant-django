2bd811f30a763aaa3382a752c1547324149f8b87
jldpDev <joseluisdp297@gmail.com>
2025-10-11 14:36:27 +0100

    Fix error with self

--- restaurant/orders/forms.py
+++ restaurant/orders/forms.py
@@ -7,13 +7,14 @@ from customers.models import CustomerProfile
 # Create the form with personal customer data
 
 class OrderForm(forms.ModelForm):
-    model = CustomerProfile
-    fields = ['first_name', 'street', 'postal_code', 'city', 'phone_number'] # field in the model
+    class Meta:
+        model = CustomerProfile
+        fields = ['first_name', 'street', 'postal_code', 'city', 'phone_number'] # field in the model
 
     #  constructor to render the form with values from the customer
     def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-        if self.isinstance:
+        super().__init__(*args, **kwargs)
+        if self.instance:
             for field_name, field in self.fields.items():
                 value = getattr(self.instance, field_name)
                 field.widget.attrs['value'] = value
