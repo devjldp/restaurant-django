d2c9cde600af75551e9c022b629baaca761b98e5
jldpDev <joseluisdp297@gmail.com>
2025-10-18 11:03:44 +0100

    Add url in order urls

--- restaurant/orders/urls.py
+++ restaurant/orders/urls.py
@@ -10,4 +10,8 @@ app_name = 'orders'
 # Define the url patterns
 urlpatterns = [
     path('', views.create_order, name='order'),
+    path('pay/<int:order_id>/', views.pay_order, name='pay_order'),
+    path('success/<int:order_id>/', views.payment_succes, name= 'payment_success'),
+    path('cancel/<int:order_id>/', views.payment_cancel, name='payment_cancel')
+
 ]
